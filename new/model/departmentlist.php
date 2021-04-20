c20456909e69b51f187ba600cf34b62c6fe046ab
Amira <azaharmyra95@gmail.com>
2021-04-20 17:27:39 +0800

    Update template with new 1. havent finish yet

---
+++ new/model/departmentlist.php
@@ -0,0 +1,131 @@
+<?php
+//Add school department
+if (isset($_POST['AddDepartmentFormSubmit']))
+{
+  $varschoolID = strval($_SESSION["loggeduser_schoolID"]);
+  $vardepartment = $_POST['txtdepartment'];
+  $bulk = new MongoDB\Driver\BulkWrite(['ordered' => TRUE]);
+  $bulk->insert(['School_id'=>$varschoolID,'DepartmentName'=> $vardepartment]);
+  $writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 1000);
+  try
+  {
+    $result =$GoNGetzDatabase->executeBulkWrite('GoNGetzSmartSchool.SchoolsDepartment', $bulk, $writeConcern);
+  }
+  catch (MongoDB\Driver\Exception\BulkWriteException $e)
+  {
+    $result = $e->getWriteResult();
+    // Check if the write concern could not be fulfilled
+    if ($writeConcernError = $result->getWriteConcernError())
+    {
+        printf("%s (%d): %s\n",
+            $writeConcernError->getMessage(),
+            $writeConcernError->getCode(),
+            var_export($writeConcernError->getInfo(), true)
+        );
+    }
+    // Check if any write operations did not complete at all
+    foreach ($result->getWriteErrors() as $writeError)
+    {
+        printf("Operation#%d: %s (%d)\n",
+            $writeError->getIndex(),
+            $writeError->getMessage(),
+            $writeError->getCode()
+        );
+    }
+  }
+  catch (MongoDB\Driver\Exception\Exception $e)
+  {
+    printf("Other error: %s\n", $e->getMessage());
+    exit;
+  }
+  printf("Matched: %d\n", $result->getMatchedCount());
+  printf("Updated  %d document(s)\n", $result->getModifiedCount());
+}
+
+//Edit school department
+if (isset($_POST['EditDepartmentFormSubmit']))
+{
+  $varschoolID = strval($_SESSION["loggeduser_schoolID"]);
+  $vardepartmentid = $_POST['txtdepartmentid'];
+  $vardepartmentname = $_POST['txtdepartmentname'];
+  $bulk = new MongoDB\Driver\BulkWrite(['ordered' => TRUE]);
+  $bulk->update(['_id' => new \MongoDB\BSON\ObjectID($vardepartmentid)],
+                ['$set' => ['DepartmentName'=>$vardepartmentname]]
+               );
+  $writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 1000);
+  try
+  {
+    $result = $GoNGetzDatabase->executeBulkWrite('GoNGetzSmartSchool.SchoolsDepartment', $bulk, $writeConcern);
+  }
+  catch (MongoDB\Driver\Exception\BulkWriteException $e)
+  {
+    $result = $e->getWriteResult();
+    // Check if the write concern could not be fulfilled
+    if ($writeConcernError = $result->getWriteConcernError())
+    {
+        printf("%s (%d): %s\n",
+            $writeConcernError->getMessage(),
+            $writeConcernError->getCode(),
+            var_export($writeConcernError->getInfo(), true)
+        );
+    }
+    // Check if any write operations did not complete at all
+    foreach ($result->getWriteErrors() as $writeError)
+    {
+        printf("Operation#%d: %s (%d)\n",
+            $writeError->getIndex(),
+            $writeError->getMessage(),
+            $writeError->getCode()
+        );
+    }
+  }
+  catch (MongoDB\Driver\Exception\Exception $e)
+  {
+    printf("Other error: %s\n", $e->getMessage());
+    exit;
+  }
+  printf("Matched: %d\n", $result->getMatchedCount());
+  printf("Updated  %d document(s)\n", $result->getModifiedCount());
+}
+
+//Delete school department
+if (isset($_POST['DeleteDepartmentFormSubmit']))
+{
+  $vardepartmentid = $_POST['txtdepartmentid'];
+  $bulk = new MongoDB\Driver\BulkWrite;
+  $bulk->delete(['_id'=>new \MongoDB\BSON\ObjectID($vardepartmentid)], ['limit' => 1]);
+  $writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 1000);
+  try
+  {
+    $result =$GoNGetzDatabase->executeBulkWrite('GoNGetzSmartSchool.SchoolsDepartment', $bulk, $writeConcern);
+  }
+  catch (MongoDB\Driver\Exception\BulkWriteException $e)
+  {
+    $result = $e->getWriteResult();
+    // Check if the write concern could not be fulfilled
+    if ($writeConcernError = $result->getWriteConcernError())
+    {
+        printf("%s (%d): %s\n",
+            $writeConcernError->getMessage(),
+            $writeConcernError->getCode(),
+            var_export($writeConcernError->getInfo(), true)
+        );
+    }
+    // Check if any write operations did not complete at all
+    foreach ($result->getWriteErrors() as $writeError)
+    {
+        printf("Operation#%d: %s (%d)\n",
+            $writeError->getIndex(),
+            $writeError->getMessage(),
+            $writeError->getCode()
+        );
+    }
+  }
+  catch (MongoDB\Driver\Exception\Exception $e)
+  {
+    printf("Other error: %s\n", $e->getMessage());
+    exit;
+  }
+  printf("Matched: %d\n", $result->getMatchedCount());
+  printf("Deleted  %d document(s)\n", $result->getModifiedCount());
+}
