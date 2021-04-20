e7cebdc8a1ea7d2b2f609b2fb153d29b6874dc8d
Amira <azaharmyra95@gmail.com>
2021-04-20 13:14:52 +0800

    delete unnecesarry file

---
+++ new/view/schoolabout.php
@@ -0,0 +1,39 @@
+<?php include ('model/schoolabout.php'); ?>
+<br><br>
+<div class="myDiv" style="color:#404040;text-align:center">
+  <br><h1 style="color:#404040;">About Us</h1>
+</div>
+<br>
+<div class="table-responsive">
+  <table class="table table-bordered table-sm" style="background-color:#ffffff;">
+    <thead class="table-light">
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row" class="table-secondary">School Name</th>
+        <td class="table-secondary"><?php echo $_SESSION["loggeduser_schoolName"] ?> </td>
+      </tr>
+      <tr>
+      <th scope="row">School Phone</th>
+      <td><?php print_r($SchoolsPhoneNo); ?></td>
+      </tr>
+      <tr>
+        <th scope="row">School address</th>
+        <td><?php echo $_SESSION["loggeduser_schoolsAddress"] ?></td>
+      </tr>
+      <tr>
+        <th scope="row">School Email</th>
+      <td><?php echo $_SESSION["loggeduser_SchoolsEmail"] ?></td>
+      </tr>
+      <tr>
+        <th scope="row">Update</th>
+        <td>
+          <button type="button"  class="btn btn-success" data-bs-toggle="modal" data-bs-target="#EditSchoolModal">
+           <i class="fa fa-edit" style="font-size:20px"></i>
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+<?php include ('view/modal-schoolabout.php'); ?>
---
+++ old/connections/db.php
@@ -0,0 +1,4 @@
+<?php
+  $GoNGetzConnectionString="mongodb://admin:TempPassword@124.217.235.244:27017/gngoffice?authSource=admin";
+  $GoNGetzDatabase = new MongoDB\Driver\Manager($GoNGetzConnectionString);
+?>
