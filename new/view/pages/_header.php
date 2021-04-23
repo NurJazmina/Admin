e0bff8cd1c3c57c8a1f6afa44c86c60bb29078fa
Amira <azaharmyra95@gmail.com>
2021-04-23 17:33:50 +0800

    delete unnecesarry file

--- new/view/pages/_header.php
+++ new/view/pages/_header.php
@@ -2,8 +2,6 @@
 <!--begin::Header-->
 <div id="kt_header" class="header header-fixed">
 
-
-
 	<!--begin::Container-->
 	<div class="container-fluid d-flex align-items-stretch justify-content-between">
 		<div></div>
