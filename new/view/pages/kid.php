f47d62afa991f46491c0f961f103a719bfe15bb1
diana <dianaa.rhahayu@gmail.com>
2021-04-26 15:32:42 +0800

    updated model-news.php

--- new/view/pages/kid.php
+++ new/view/pages/kid.php
@@ -1,42 +1,41 @@
-<form class="form">
-    <div class="card card-custom gutter-b">
-    <div class="card-body">
+<form action="index.php?page=news" method="post" name="AddNews"><br><br>
+    <div class="table-responsive" style="width:100%; margin:0 auto; padding: 40px">
+        <div class="card card-custom gutter-b">
+        
+        <div class="card-body">
+        
+        <div class="form-group row">
+            <label class="col-lg-2 col-form-label text-lg-left"><h5>TITLE</h5></label>
+                <div class="col-lg-8">
+                    <input type="text" class="form-control" id="staticStaffNo" name="txttitle" size="200" oninput="let p=this.selectionStart;this.value=this.value.toUpperCase();this.setSelectionRange(p, p);" required>   
+                </div>
+        </div>
+        <div class="form-group row">
+            <label class="col-lg-2 col-form-label text-lg-left"><h5>ACCESS TYPE</h5></label>
+                <div class="col-lg-8">
+                <select class="form-control" id="staticStaffNo" name="access" required>
+                    <option>Public</option>
+                    <option>Staff</option>
+                    <option>Teacher</option>
+                    <option>Parents</option>
+                </select>
+                </div>
+        </div>
 
-    <div class="form-group row">
-        <label class="col-lg-2 col-form-label text-lg-left">TITLE</label>
-            <div class="col-lg-7">
-                <input type="email" class="form-control" placeholder="Enter title here"/>
-            </div>
+        <div>
+        <label><h5> NEWS </h5></label>
+            <textarea id="basic-example" name="txtdetail" ></textarea>
+        </div>
+
+        <div class="card-footer">
+            <div class="row">
+            <div class="col-lg-2"></div>
+        <div>
+            <button type="submit" class="btn btn-success" name="AddNews">Confirm</button>
+            <button type="button"  class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+        </div>
+
+        </div>  
+        </div>
     </div>
-
-    <div class="form-group row">
-        <label class="col-lg-2 col-form-label text-lg-left">ACCESS TYPE</label>
-            <div class="col-lg-7">
-                <select class="form-control" id="exampleSelect1">
-                <option>Public</option>
-                <option>Staff</option>
-                <option>Teacher</option>
-                <option>Parents</option>
-            </select>
-            </div>
-    </div>
-    <div>
-      <label><strong> News </strong></label>
-        <textarea id="basic-example" name="txtdetail" ></textarea>
-    </div>
-
-
-
-
-    
-
- <div class="card-footer">
-  <div class="row">
-   <div class="col-lg-2"></div>
-   <div>
-    <button type="reset" class="btn btn-success mr-2">Submit</button>
-    <button type="reset" class="btn btn-secondary">Cancel</button>
-   </div>
-  </div>
- </div>
 </form>
--- new/view/pages/modal-news.php
+++ new/view/pages/modal-news.php
@@ -1,36 +1,41 @@
 <form action="index.php?page=news" method="post" name="AddNews"><br><br>
-<div class="table-responsive" style="width:100%; margin:0 auto;">
-  <table class="table table-bordered dt-responsive nowrap table-sm table-success" cellpadding="1" cellspacing="0" style="font-family: Arial; color:#505050; font-size: 15px;">
-    <tbody>
-      <tr> 
-        <td height="27"><strong>TITLE</strong></td>
-        <td>
-        <input type="text" class="form-control" id="staticStaffNo" name="txttitle" size="200" oninput="let p=this.selectionStart;this.value=this.value.toUpperCase();this.setSelectionRange(p, p);" required>
-        </td>
-      </tr>
-      <tr> 
-        <td><strong>ACCESS TYPE</strong></td>
-        <td>
-        <select class="form-control" id="staticStaffNo" name="access" required>
-          <option value="PUBLIC">PUBLIC</option>
-          <option value="SCHOOL1">STAFF</option>
-          <option value="SCHOOL0">TEACHER</option>
-          <option value="VIP">PARENT</option>
-        </select>
-        </td>
-      </tr>
-      <tr> 
-        <td colspan="2"><strong>NEWS</strong><br><br>
-        <textarea id="basic-example" name="txtdetail" ></textarea>
-        </td>
-      </tr> 
-      <tr> 
-        <td height="27" colspan="2"><div align="right"> 
-            <button type="submit" class="btn btn-secondary" name="AddNews">Confirm</button>
+    <div class="table-responsive" style="width:100%; margin:0 auto; padding: 40px">
+        <div class="card card-custom gutter-b">
+        
+        <div class="card-body">
+        
+        <div class="form-group row">
+            <label class="col-lg-2 col-form-label text-lg-left"><h5>TITLE</h5></label>
+                <div class="col-lg-8">
+                    <input type="text" class="form-control" id="staticStaffNo" name="txttitle" size="200" oninput="let p=this.selectionStart;this.value=this.value.toUpperCase();this.setSelectionRange(p, p);" required>   
+                </div>
+        </div>
+        <div class="form-group row">
+            <label class="col-lg-2 col-form-label text-lg-left"><h5>ACCESS TYPE</h5></label>
+                <div class="col-lg-8">
+                <select class="form-control" id="staticStaffNo" name="access" required>
+                    <option>Public</option>
+                    <option>Staff</option>
+                    <option>Teacher</option>
+                    <option>Parents</option>
+                </select>
+                </div>
+        </div>
+
+        <div>
+        <label><h5> NEWS </h5></label>
+            <textarea id="basic-example" name="txtdetail" ></textarea>
+        </div>
+
+        <div class="card-footer">
+            <div class="row">
+            <div class="col-lg-2"></div>
+        <div>
+            <button type="submit" class="btn btn-success" name="AddNews">Confirm</button>
             <button type="button"  class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+        </div>
+
+        </div>  
+        </div>
+    </div>
 </form>
