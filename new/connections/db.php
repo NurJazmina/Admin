4e8ef6b2d79ceed6db46f1a9aaf9153306e410e7
Amira <azaharmyra95@gmail.com>
2021-04-27 09:58:20 +0800

    database merge

--- new/connections/db.php
+++ new/connections/db.php
@@ -1,14 +1,9 @@
 <?php
-<<<<<<< HEAD
-  
   //database server//
   $GoNGetzConnectionString="mongodb://admin:TempPassword@51.79.173.45:27017/gngoffice?authSource=admin";
 
   //production server//
   //$GoNGetzConnectionString="mongodb://admin:TempPassword@124.217.235.244:27017/gngoffice?authSource=admin";
-=======
-  //$GoNGetzConnectionString="mongodb://admin:TempPassword@51.79.173.45:27017/gngoffice?authSource=admin";
-  $GoNGetzConnectionString="mongodb://admin:TempPassword@51.79.173.45:27017/gngoffice?authSource=admin";
->>>>>>> 5c6f3487f0bce238ab6e344ca603b9677e720b0b
+
   $GoNGetzDatabase = new MongoDB\Driver\Manager($GoNGetzConnectionString);
 ?>
