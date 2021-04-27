602c5da135d925b49cc578577dc1386e51e0b201
Amira <azaharmyra95@gmail.com>
2021-04-27 10:17:52 +0800

    add error page

--- new/controller/page.php
+++ new/controller/page.php
@@ -1,6 +1,7 @@
 
 <?php
-    if(!isset($_SESSION['loggeduser_id']) && empty($_SESSION['loggeduser_id'])) {
+    if(!isset($_SESSION['loggeduser_id']) && empty($_SESSION['loggeduser_id'])) 
+    {
     ?>
         <div class="row" style="height:100vh;">
             <div class="col-sm-12 col-lg-6" style="background-image:url(images/loginpagebg.jpg); background-repeat:no-repeat; background-size:cover;">
@@ -8,11 +9,15 @@
                 <?php include 'view/login.php'; ?>
         </div>
     <?php
-    } else {
-        if (!isset($_GET['page']) || empty($_GET['page'])){
+    } 
+    else 
+    {
+        if (!isset($_GET['page']) || empty($_GET['page']))
+        {
             include 'view/pages/dashboard.php';
         }
-        else {
+        else 
+        {
             include 'view/pages/'.$_GET['page'].'.php';
         }
     }
