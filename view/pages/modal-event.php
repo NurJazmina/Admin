1e0fa27e2fa6f858a2f0f3318cf285a30c34c13f
diana <dianaa.rhahayu@gmail.com>
2021-05-07 17:18:37 +0800

    style: help-tip modal added

--- view/pages/modal-event.php
+++ view/pages/modal-event.php
@@ -1,3 +1,89 @@
+<style>
+
+.help-tip{
+    position: absolute;
+    top: 18px;
+    right: 18px;
+    text-align: center;
+    background-color: #BCDBEA;
+    border-radius: 50%;
+    width: 24px;
+    height: 24px;
+    font-size: 14px;
+    line-height: 26px;
+    cursor: default;
+}
+
+.help-tip:before{
+    content:'?';
+    font-weight: bold;
+    color:#fff;
+}
+
+.help-tip:hover p{
+    display:block;
+    transform-origin: 100% 0%;
+
+    -webkit-animation: fadeIn 0.3s ease-in-out;
+    animation: fadeIn 0.3s ease-in-out;
+
+}
+
+.help-tip p{    /* The tooltip */
+    display: none;
+    text-align: left;
+    background-color: #1E2021;
+    padding: 20px;
+    width: 300px;
+    position: absolute;
+    border-radius: 3px;
+    box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.2);
+    right: -4px;
+    color: #FFF;
+    font-size: 13px;
+    line-height: 1.4;
+}
+
+.help-tip p:before{ /* The pointer of the tooltip */
+    position: absolute;
+    content: '';
+    width:0;
+    height: 0;
+    border:6px solid transparent;
+    border-bottom-color:#1E2021;
+    right:10px;
+    top:-12px;
+}
+
+.help-tip p:after{ /* Prevents the tooltip from being hidden */
+    width:100%;
+    height:40px;
+    content:'';
+    position: absolute;
+    top:-40px;
+    left:0;
+}
+
+/* CSS animation */
+
+@-webkit-keyframes fadeIn {
+    0% { 
+        opacity:0; 
+        transform: scale(0.6);
+    }
+
+    100% {
+        opacity:100%;
+        transform: scale(1);
+    }
+}
+
+@keyframes fadeIn {
+    0% { opacity:0; }
+    100% { opacity:100%; }
+}
+</style>
+
 <?php
 $_SESSION["title"] = "Event";
 ?>
@@ -17,8 +103,8 @@ $_SESSION["title"] = "Event";
         </div>
         <div class="form-group row">
             <label class="col-lg-2 col-form-label text-lg-left"><h5>ACCESS TYPE</h5></label>
-                <div class="col-lg-8">
-                <select class="form-control" id="staticStaffNo" name="txtaccess" style="height: auto; width: 50%" required>
+                <div class="col-lg-6">
+                <select class="form-control" id="staticStaffNo" name="txtaccess" style="height: auto;" required>
                   <option value="PUBLIC">PUBLIC</option>
                   <option value="SCHOOL1">STAFFS</option>
                   <option value="SCHOOL0">TEACHERS</option>
@@ -28,14 +114,23 @@ $_SESSION["title"] = "Event";
         </div>
         <div class="form-group row">
             <label class="col-lg-2 col-form-label text-lg-left"><h5>VENUE</h5></label>
-                <div class="col-lg-8">
+                <div class="col-lg-6">
                     <input type="text" class="form-control"  id="staticStaffNo" name="txtschoolEventVenue">   
                 </div>
         </div>
         <div class="form-group row">
-            <label class="col-lg-2 col-form-label text-lg-left"><h5>LOCATION</h5></label>
-                <div class="col-lg-8">
-                    <input type="text" class="form-control"  id="staticStaffNo" name="txtschoolEventLocation">
+            <label class="col-lg-2 col-form-label text-lg-left"><h5>ADDRESS</h5></label>
+                <div class="col-lg-3">
+                    <input type="text" class="form-control"  id="staticStaffNo" name="txtschoolLocation">
+                </div>
+        </div>
+        <div class="form-group row">
+            <label class="col-lg-2 col-form-label text-lg-left"><h5>GOOGLE MAPS LINK</h5></label>
+            <div class="help-tip">
+                <p>Testing</p>
+            </div>
+                <div class="col-lg-3">
+                    <input type="text" class="form-control"  id="staticStaffNo" name="txtschoolGoogleMaps">
                 </div>
         </div>
         <div class="form-group row">
@@ -44,7 +139,7 @@ $_SESSION["title"] = "Event";
                     <input type="datetime-local" id="staticStaffNo" name="txtSchoolEventDateStart">
                 </div>
         </div>
-        <div class="form-group row">
+        <div class="form-group row"> 
             <label class="col-lg-2 col-form-label text-lg-left"><h5>EVENT END</h5></label>
                 <div class="col-lg-8">
                     <input type="datetime-local" id="staticStaffNo" name="txtSchoolEventDateEnd">
