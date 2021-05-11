29f1f419eb3c52e98131b0ce0434df080f2d5108
Amira <azaharmyra95@gmail.com>
2021-05-11 16:06:55 +0800

    fix: styling modal-event

--- view/pages/modal-event.php
+++ view/pages/modal-event.php
@@ -22,13 +22,13 @@ $_SESSION["title"] = "Event";
         
         <div class="form-group row">
             <label class="col-lg-2 col-form-label text-lg-left"><h5>TITLE</h5></label>
-                <div class="col-lg-8">
-                    <input type="text" class="form-control" id="staticStaffNo" name="txttitle" size="200" oninput="let p=this.selectionStart;this.value=this.value.toUpperCase();this.setSelectionRange(p, p);" required>   
+                <div class="col-lg-10">
+                    <input type="text" class="form-control" id="staticStaffNo" name="txttitle"  oninput="let p=this.selectionStart;this.value=this.value.toUpperCase();this.setSelectionRange(p, p);" required>   
                 </div>
         </div>
         <div class="form-group row">
             <label class="col-lg-2 col-form-label text-lg-left"><h5>ACCESS TYPE</h5></label>
-                <div class="col-lg-6">
+                <div class="col-lg-10">
                 <select class="form-control" id="staticStaffNo" name="txtaccess" style="height: auto;" required>
                   <option value="PUBLIC">PUBLIC</option>
                   <option value="STAFF">STAFFS</option>
@@ -39,13 +39,13 @@ $_SESSION["title"] = "Event";
         </div>
         <div class="form-group row">
             <label class="col-lg-2 col-form-label text-lg-left"><h5>VENUE</h5></label>
-                <div class="col-lg-6">
+                <div class="col-lg-10">
                     <input type="text" class="form-control"  id="staticStaffNo" name="txtschoolEventVenue" required>   
                 </div>
         </div>
         <div class="form-group row">
             <label class="col-lg-2 col-form-label text-lg-left"><h5>ADDRESS</h5></label>
-                <div class="col-lg-3">
+                <div class="col-lg-10">
                     <input type="text" class="form-control"  id="staticStaffNo" name="txtschoolEventAddress" required>
                 </div>
         </div>
@@ -61,30 +61,29 @@ $_SESSION["title"] = "Event";
                 </div>
             </h5>
             </label>
-                <div class="col-lg-3">
+                <div class="col-lg-10">
                     <input type="text" class="form-control"  id="staticStaffNo" name="txtschoolEventLocation" required>
                 </div>
         </div>
         <div class="form-group row">
             <label class="col-lg-2 col-form-label text-lg-left"><h5>EVENT START</h5></label>
-                <div class="col-lg-8">
-                    <input type="datetime-local" id="staticStaffNo" name="txtSchoolEventDateStart">
+                <div class="col-lg-10">
+                    <input type="datetime-local" class="form-control" id="staticStaffNo" name="txtSchoolEventDateStart">
                 </div>
         </div>
         <div class="form-group row"> 
             <label class="col-lg-2 col-form-label text-lg-left"><h5>EVENT END</h5></label>
-                <div class="col-lg-8">
-                    <input type="datetime-local" id="staticStaffNo" name="txtSchoolEventDateEnd">
+                <div class="col-lg-10">
+                    <input type="datetime-local" class="form-control" id="staticStaffNo" name="txtSchoolEventDateEnd">
                 </div>
         </div>
         <div class="card-footer">
-            <div class="row">
+        <div class="row">
             <div class="col-lg-2"></div> 
-        <div class="text-right">
-            <button type="submit" class="btn btn-success" name="AddNews">Confirm</button>
-            <button type="button"  class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-        </div>
-
+            <div class="text-right">
+                <button type="submit" class="btn btn-success" name="AddNews">Confirm</button>
+                <button type="button"  class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+            </div>
         </div>  
         </div>
     </div>
