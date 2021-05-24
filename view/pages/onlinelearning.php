3e32ffb77f50639419d1b0b7f2ab0927d0e8dfb4
diana <dianaa.rhahayu@gmail.com>
2021-05-24 15:12:30 +0800

    Fix: Kid.php changed to onlinelearning.php

---
+++ view/pages/onlinelearning.php
@@ -0,0 +1,77 @@
+<?php
+$_SESSION["title"] = "Online learning";
+?>
+<?php include 'view/partials/_subheader/subheader-v1.php'; ?>
+
+<html>
+    <body>
+        (Testing)
+    <br><br>
+    <div class="card card-custom gutter-b">
+        <div class="card-header">
+            <div class="card-title">
+                <h3 class="card-label"> Notes </div>
+            </div>
+        
+        <div class="row">
+            <div class="col-lg-4">
+                <!--begin::Card-->
+                <div class="card card-custom card-stretch">
+                    <div class="card-header">
+                        <div class="card-title">
+                        <h5 class="card-label">Subject</h5>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        Content one 
+                    </div>
+                </div>
+                <!--end::Card-->
+            </div>
+
+            <div class="col-lg-4">
+                <!--begin::Card-->
+                <div class="card card-custom card-stretch">
+                    <div class="card-header">
+                        <div class="card-title">
+                            <h5 class="card-label">Subject</h5>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        Content two
+                    </div>
+                </div>
+                <!--end::Card-->
+            </div>
+
+            <div class="col-lg-4">
+                <!--begin::Card-->
+                <div class="card card-custom card-stretch">
+                    <div class="card-header">
+                        <div class="card-title">
+                            <h5 class="card-label">Subject</h5>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        Content three
+                    </div>
+                </div>
+                <!--end::Card-->
+            </div>
+        </div>
+
+    </div>
+    <div class="card card-custom gutter-b">
+        <div class="card-header">
+            <div class="card-title">
+                <h3 class="card-label">Exercises and quiz </div>
+        </div>
+        <div class="card-body">
+            Display view for exercises. Teachers are able to add/create questions through here. There will be a button to add or delete.
+        </div>
+    </div>
+
+
+
+    </body>
+</html>
