e72323e02f81cc449fbe79f362fae56fe0ee7fa9
diana <dianaa.rhahayu@gmail.com>
2021-05-25 17:22:09 +0800

    Style: Exercise page layout changed.

--- view/pages/exercises.php
+++ view/pages/exercises.php
@@ -12,26 +12,81 @@ $_SESSION["title"] = "Online learning";
     <body>
     <form id="AddNewExercise" name="AddNewExercise" action="index.php?page=onlinelearning" method="post">
         <div id="AddExerciseModal" tabindex="-1" aria-labelledby="AddExerciseModalLabel" aria-hidden="true">
+
             <div class="modal-dialog modal-lg modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="AddExerciseModalLabel">New Exercise</h5> 
+                        <h5 class="modal-title" id="AddExerciseModalLabel">New Exercise/Quiz</h5> 
                     </div>
+
                     <div class="modal-body">
-                    <div class="form-group row">
-                    <div class="form-group">
-                        <label for="AddNewExercises">Add new exercises: </label>
-                            <textarea class="form-control rounded-0" id="AddNewExercises" rows="10"></textarea>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-primary" name="AddExerciseSubmit" onclick="alert('Questions added!')">Add</button>
-                    </div>
+                        <div class="form-group row">
+                                <label class="col-lg-2 col-form-label text-lg-left"><h5>Selection:</h5></label>
+                                <div class="col-lg-6">
+                                    <select class="form-control" id="questionType" style="height: auto;" name="access" required>
+                                    <option value="Objective">Objective</option>
+                                    <option value="Subjective">Subjective</option>
+                                    </select>
+                                </div>
+                        </div>
                     </div>
                 </div>
             </div>
+
+            <div class="modal-dialog modal-lg modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="objective">Objective</h5> 
+                    </div>
+                    <div class="modal-body">
+                    <div class="form-group row">
+                            <label class="col-lg-2 col-form-label text-lg-left"><h5>Title:</h5></label>
+                            <div class="col-lg-8">
+                                <input name="title" type="text" class="form-control" id="mcqQuestion" placeholder="Enter your title here" name="txttitle" size="200" oninput="let p=this.selectionStart;this.value=this.value.toUpperCase();this.setSelectionRange(p, p);">   
+                            </div>
+                    </div>
+                        <div class="form-group row">
+                        
+                        <!-- test -->
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Add</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="modal-dialog modal-lg modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="subjective">Subjective</h5> 
+                    </div>
+                    <div class="modal-body">
+                    <div class="form-group row">
+                            <label class="col-lg-2 col-form-label text-lg-left"><h5>Title:</h5></label>
+                            <div class="col-lg-8">
+                                <input name="title" type="text" class="form-control" id="subjectiveQuestion" placeholder="Enter your title here" name="txttitle" size="200" oninput="let p=this.selectionStart;this.value=this.value.toUpperCase();this.setSelectionRange(p, p);">   
+                            </div>
+                    </div>
+                    <div class="form-group row">
+                            <label for="AddNewExercises">Question goes here:</label><br><br>
+
+                    </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Add</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </form>
     </body>
 </html>
-   
+
+<!-- <div class="form-group row">
+                            <label for="AddNewExercises">Question goes here:</label><br><br>
+                                    <textarea class="form-control rounded-0" id="AddNewExercises" rows="3"></textarea>
+                    </div> -->
--- view/pages/notes.php
+++ view/pages/notes.php
@@ -13,15 +13,19 @@ $_SESSION["title"] = "Online learning";
                         <h5 class="modal-title" id="AddNotesModalLabel">New Notes</h5> 
                     </div>
                     <div class="modal-body">
-                    <div class="form-group row">
-                        <div class="form-group">
-                            <label for="AddNewNotes">Add new notes: </label>
-                                <textarea class="form-control rounded-0" id="AddNewNotes" rows="10"></textarea>
+                        <div class="form-group row">
+                        <label class="col-lg-2 col-form-label text-lg-left">Title</label>
+                            <div class="col-lg-12">
+                                <input name="title" type="text" class="form-control" name="txttitle" size="200" oninput="let p=this.selectionStart;this.value=this.value.toUpperCase();this.setSelectionRange(p, p);" required>   
+                            </div>
+                            <div class="form-group">
+                                <label for="AddNewNotes">Add new notes:</label>
+                                    <textarea class="form-control rounded-0" id="AddNewNotes" rows="10"></textarea>
+                            </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary" name="AddNotesSubmit" onclick="alert('New notes added!')">Add</button>
                         </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-primary" name="AddNotesSubmit" onclick="alert('New notes added!')">Add</button>
-                    </div>
                     </div>
                 </div>
             </div>
