457112a93287be59e611a9519638e9ffd61bc53d
Amira <azaharmyra95@gmail.com>
2021-05-27 15:18:17 +0800

    fix : testing

--- view/pages/mira.php
+++ view/pages/mira.php
@@ -1,1 +1,54 @@
-<?php include 'model/counter.php'; ?>
+<?php 
+    include ('model/mira.php');
+
+    $filter = [NULL];
+    $query = new MongoDB\Driver\Query($filter);
+    $cursor = $GoNGetzDatabase->executeQuery('GoNGetzSmartSchool.testing',$query);
+    foreach ($cursor as $document)
+    {
+      $testing_id = strval($document->_id);
+      $Date = strval($document->Date);
+      $Image = strval($document->Image);
+      ?>
+      <div class="card">
+        <div class="card-header">
+        <?php
+        echo $testing_id."<br>";
+        ?>
+        </div>
+        <div class="card-body">
+        <?php
+        echo $Date."<br>";
+        echo $Image
+        ?>
+        </div>
+      </div>
+      <?php
+    }
+    ?>
+    <form id="AddImage" name="AddImage" action="index.php?page=mira" method="post">
+    <div id="AddDepartmentModal" tabindex="-1" aria-labelledby="AddDepartmentModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h5 class="modal-title" id="AddDepartmentModalLabel">Add Image</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+            <div class="form-group row">
+                <label for="staticStaffNo" class="col-sm-2 col-form-label">New Image</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control" id="staticStaffNo" name="txtimage" oninput="let p=this.selectionStart;this.value=this.value.toUpperCase();this.setSelectionRange(p, p);">
+                </div>
+            </div>
+            </div>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-success" name="AddImage">Save changes</button>
+            </div>
+        </div>
+    </div>
+    </div>
+    </form>
+    <?php
+?>
--- view/pages/onlinelearning.php
+++ view/pages/onlinelearning.php
@@ -2,113 +2,114 @@
 $_SESSION["title"] = "Online learning";
 include 'view/partials/_subheader/subheader-v1.php'; 
 ?>
+<div class="card card-custom gutter-b">
+    <div class="card-header">
+        <div class="card-title">
+            <h3 class="card-label"> Notes </div>
+        </div>
+    </div>
+    <div class="row">
+        <?php
+        $filter = ['School_id'=>$_SESSION["loggeduser_schoolID"],'Note_sort'=>1];
+        $query = new MongoDB\Driver\Query($filter);
+        $cursor = $GoNGetzDatabase->executeQuery('GoNGetzSmartSchool.OnlineLearningNotes',$query);
+        foreach ($cursor as $document)
+        {
+            $Subject_id = $document->Subject_id;
+            $Title = $document->Title;
+            $Detail = $document->Detail;
+            $Created_by = $document->Created_by;
+            $Edited_by = $document->Edited_by;
 
-<?php
-
-?>
-    <div class="card card-custom gutter-b">
-        <div class="card-header">
-            <div class="card-title">
-                <h3 class="card-label"> Notes </div>
+            $filter = ['_id'=>new \MongoDB\BSON\ObjectId($Subject_id)];
+            $query = new MongoDB\Driver\Query($filter);
+            $cursor = $GoNGetzDatabase->executeQuery('GoNGetzSmartSchool.SchoolsSubject',$query);
+            foreach ($cursor as $document)
+            {
+                $SubjectName = $document->SubjectName;
+            }
+            ?>
+            <div class="col-lg-4">
+                <!--begin::Card-->
+                <div class="card card-custom card-stretch">
+                    <div class="card-header">
+                        <div class="card-title">
+                        <a href="index.php?page=notes"><?php echo $SubjectName; ?></a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="claimedRight"><?php echo $Title; ?> <br> <?php echo $Detail; ?></div>
+                    </div>
+                </div>
+                <!--end::Card-->
             </div>
+        <?php
+        }
+        ?>
+    </div>
+</div>
+
+<div class="card card-custom gutter-b">
+    <div class="card-header">
+        <div class="card-title">
+            <h3 class="card-label">Exercises and quiz </div>
+    </div>
+    <div class="row">
+        <?php
+        $filter = ['School_id'=>$_SESSION["loggeduser_schoolID"]];
+        $query = new MongoDB\Driver\Query($filter);
+        $cursor = $GoNGetzDatabase->executeQuery('GoNGetzSmartSchool.OnlineLearningNotes',$query);
+        foreach ($cursor as $document)
+        {
+            $Subject_id = $document->Subject_id;
+            $Note_sort = $document->Note_sort;
+
+            $filter = ['_id'=>new \MongoDB\BSON\ObjectId($Subject_id)];
+            $query = new MongoDB\Driver\Query($filter);
+            $cursor = $GoNGetzDatabase->executeQuery('GoNGetzSmartSchool.SchoolsSubject',$query);
+            foreach ($cursor as $document)
+            {
+                $SubjectName = $document->SubjectName;
+            }
+
+            $filter = ['Subject_id'=>$Subject_id];
+            $query = new MongoDB\Driver\Query($filter);
+            $cursor = $GoNGetzDatabase->executeQuery('GoNGetzSmartSchool.OnlineLearningQuestions',$query);
+            foreach ($cursor as $document)
+            {
+                $Question_id = $document->_id;
+                $Question_type = $document->Question_type;
+                $Questions = $document->Questions;
+                $Option_A = $document->Option_A;
+                $Option_B = $document->Option_B;
+                $Option_C = $document->Option_C;
+                $Option_D = $document->Option_D;
+                $Date_end = $document->Date_end;
+                $Date_start = $document->Date_start;
+            }
+        }
+        ?>
+        <div class="col-lg-4">
+            <!--begin::Card-->
+            <div class="card card-custom card-stretch">
+                <div class="card-header">
+                    <div class="card-title">
+                        <a href="index.php?page=notes"><?php echo $SubjectName; ?></a>
+                    </div>
+                </div>
+                <div class="menu-section" style="border-bottom: 1px solid #eceef7; margin-top: 12px; padding-top: 12px;">
+                    <div class="card-title" align="center">
+                        <a><?php echo "SLOT : ".$Note_sort; ?></a>
+                    </div>
+                </div>
+                <div class="card-body">
+                <a href="index.php?page=notes&id=<?php echo $Question_id ; ?>"><?php echo $Question_type; ?> <br> <?php echo $Questions; ?></a>
+                </div>
+            </div>
+            <!--end::Card-->
+        </div>
+        <?php
         
-        <div class="row">
-            <div class="col-lg-4">
-                <!--begin::Card-->
-                <div class="card card-custom card-stretch">
-                    <div class="card-header">
-                        <div class="card-title">
-                        <a href="index.php?page=notes">Subject</a>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        Content one <br> (Notes will display here according to the subject)
-                    </div>
-                </div>
-                <!--end::Card-->
-            </div>
-
-            <div class="col-lg-4">
-                <!--begin::Card-->
-                <div class="card card-custom card-stretch">
-                    <div class="card-header">
-                        <div class="card-title">
-                        <a href="index.php?page=notes">Subject</a>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        Content two <br> (Notes will display here according to the subject)
-                    </div>
-                </div>
-                <!--end::Card-->
-            </div>
-
-            <div class="col-lg-4">
-                <!--begin::Card-->
-                <div class="card card-custom card-stretch">
-                    <div class="card-header">
-                        <div class="card-title">
-                        <a href="index.php?page=notes">Subject</a>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        Content three <br> (Notes will display here according to the subject)
-                    </div>
-                </div>
-                <!--end::Card-->
-            </div>
-        </div>
-
+        ?>
     </div>
-    <div class="card card-custom gutter-b">
-        <div class="card-header">
-            <div class="card-title">
-                <h3 class="card-label">Exercises and quiz </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-4">
-                <!--begin::Card-->
-                <div class="card card-custom card-stretch">
-                    <div class="card-header">
-                        <div class="card-title">
-                        <a href="index.php?page=exercises">Subject</a>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        Content one <br> (Questions will display here according to the subject)
-                    </div>
-                </div>
-                <!--end::Card-->
-            </div>
-
-            <div class="col-lg-4">
-                <!--begin::Card-->
-                <div class="card card-custom card-stretch">
-                    <div class="card-header">
-                        <div class="card-title">
-                        <a href="index.php?page=exercises">Subject</a>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        Content two <br> (Questions will display here according to the subject)
-                    </div>
-                </div>
-                <!--end::Card-->
-            </div>
-
-            <div class="col-lg-4">
-                <!--begin::Card-->
-                <div class="card card-custom card-stretch">
-                    <div class="card-header">
-                        <div class="card-title">
-                        <a href="index.php?page=exercises">Subject</a>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        Content three <br> (Questions will display here according to the subject)
-                    </div>
-                </div>
-                <!--end::Card-->
-            </div>
-        </div>
-    </div>
+</div>
