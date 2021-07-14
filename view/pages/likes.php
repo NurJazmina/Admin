e930895fc607a7219e71f850db1ad6ca4568ee26
Amira <azaharmyra95@gmail.com>
2021-07-14 11:25:49 +0800

    feat : online learning like button in php using ajax

--- view/pages/likes.php
+++ view/pages/likes.php
@@ -1,16 +1,76 @@
 <?php
+include '../../connections/db.php';
 
-$existingname = array("Daniel", "Dennis", "Danny", "Jane");
+if (isset($_POST['like']))
+{
+    $URL_LIKES = "$_SERVER[REQUEST_URI]";
+    $url = "";
 
-if (isset($_POST['like'])){
-    $name = $_POST['like'];
-    foreach ($existingname as $newname){
-        if(stripos($newname, $name) !== false){
-            echo $newname;
-            echo "<br>";
+    $like = $_POST['like'];
+    echo $like ;
+
+    if ($like == 1)
+    {
+        $Consumer_id = $_POST['Consumer_id'];
+        $filter = ['url'=>$URL_LIKES];
+        $query = new MongoDB\Driver\Query($filter);
+        $cursor = $GoNGetzDatabase->executeQuery('GoNGetzSmartSchool.Likes',$query);
+        foreach ($cursor as $document)
+        {
+            $url = strval($document->url);
+        }
+        if ($url == "")
+        {
+            $array = [];
+            $arraycount =[ 'Consumer_id'=>$Consumer_id];
+            array_push($array, $arraycount);
+            $bulk = new MongoDB\Driver\BulkWrite(['ordered' => TRUE]);
+            $bulk->insert(['url'=>$URL_LIKES,
+                            'Consumer'=>$array
+                          ]);
+            $writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 1000);
+            $result =$GoNGetzDatabase->executeBulkWrite('GoNGetzSmartSchool.Likes', $bulk, $writeConcern);
+        }
+        elseif ($url == $URL_LIKES)
+        {
+            $bulk = new MongoDB\Driver\BulkWrite(['ordered' => TRUE]);
+            $bulk->update(
+                            ['url' => $URL_LIKES],
+                            [
+                                '$push'=>
+                                [
+                                    'Consumer'=>
+                                    [
+                                        'Consumer_id' => $Consumer_id
+                                    ]
+                                ],
+                            ],
+                            ['upsert' => TRUE]
+                            );
+            $writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 1000);
+            $result =$GoNGetzDatabase->executeBulkWrite('GoNGetzSmartSchool.Likes', $bulk, $writeConcern);
         }
     }
+    elseif($like == 0)
+    {
+        $Consumer_id = $_POST['Consumer_id'];
+        $bulk = new MongoDB\Driver\BulkWrite(['ordered' => TRUE]);
+        $bulk->update(
+                        ['url' => $URL_LIKES],
+                        [
+                            '$pull'=>
+                            [
+                                'Consumer'=>
+                                [
+                                    'Consumer_id' => $Consumer_id
+                                ]
+                            ],
+                        ],
+                        ['upsert' => TRUE]
+                        );
+        $writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 1000);
+        $result =$GoNGetzDatabase->executeBulkWrite('GoNGetzSmartSchool.Likes', $bulk, $writeConcern);
+       
+    }
 }
-
-
 ?>
--- view/pages/mira.php
+++ view/pages/mira.php
@@ -1,19 +1,53 @@
-
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 <script>
-$(document).ready(function(){
+$(document).ready(function() {
 
-    $("input").keyup(function(){
-        var name = $("input").val();
-        $.post("view/pages/likes.php", {
-            like: name
-        }, function(data, status){
-            $("#test").html(data);
-        });
+  var toggleText = $("#ip").val();
+  var name = $("#name").val();
+  $("#ipclear").click(function() {
+        if ($("#ip").val() != '1') 
+        {
+            toggleText = $("#ip").val();
+
+            $.post("view/pages/likes.php", {
+            like: '1',
+            Consumer_id: name
+            },
+            function(data, status){
+                $("#test").html(data);
+            },
+            );
+            $("#ip").val('1');
+            $("#ip").prop("disabled", false);
+                $(this).removeClass('btn-light').addClass('btn-success');
+        }
+        else
+        {
+            $("#ip").val(toggleText);
+
+            $.post("view/pages/likes.php", {
+            like: '0',
+            Consumer_id: name
+            }, 
+            function(data, status){
+                $("#test").html(data);
+            });
+
+            $("#ip").prop("disabled", false);
+                $(this).removeClass('btn-success').addClass('btn-light');
+        }
     });
 
 });
 </script>
-
-<input type="text" name="name" class="form-control">
-<p id="test"></p>
+<div class="input-group">
+    <div class="row">
+        <div class="col-sm-12">
+            <input type="hidden" id="ip" value="0">
+            <input type="hidden" id="name" value="<?php echo $_SESSION["loggeduser_id"]; ?>">
+            <button type="button" class="btn far fa-kiss-wink-heart icon-xl" id="ipclear">
+                <a id="test"></a>
+            </button>
+        </div>
+    </div>
+</div>
